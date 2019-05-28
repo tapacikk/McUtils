5e2b32479d1ac83d2e319c17e1ba98f6f8d13c7b
b3m2a1 <b3m2a1@gmail.com>
2019-05-28 14:40:37 -0700

    Update ConstantsData.py

--- Data/ConstantsData.py
+++ Data/ConstantsData.py
@@ -2,4 +2,4 @@
 from .CommonData import DataHandler
 
 # this might become a singleton class at some point in the future...
-AtomData = DataHandler("ConstantsData")
+ConstantsData = DataHandler("ConstantsData")
