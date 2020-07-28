Little bug fixes to derivative code

Coordinerds/CoordinateSystems/CartesianToZMatrix.py
@@ -73,7 +73,7 @@ class CartesianToZMatrixConverter(CoordinateSystemConverter):
     def get_diheds(points, centers, seconds, thirds):
         return pts_dihedrals(points, centers, seconds, thirds)
 
-    def convert_many(self, coords, ordering = None, use_rad=True, return_derivs=False, **kw):
+    def convert_many(self, coords, ordering=None, use_rad=True, return_derivs=False, **kw):
         """
         We'll implement this by having the ordering arg wrap around in coords?
         """
@@ -81,9 +81,15 @@ class CartesianToZMatrixConverter(CoordinateSystemConverter):
             ordering = range(len(coords[0]))
         base_shape = coords.shape
         new_coords = np.reshape(coords, (np.product(base_shape[:-1]),) + base_shape[-1:])
-        new_coords, ops = self.convert(new_coords, ordering=ordering, use_rad=use_rad)
-        new_shape = base_shape[:-2] + (base_shape[-2]-1, new_coords.shape[-1])
+        new_coords, ops = self.convert(new_coords, ordering=ordering, use_rad=use_rad, return_derivs=return_derivs)
+        single_coord_shape = (base_shape[-2]-1, new_coords.shape[-1])
+        new_shape = base_shape[:-2] + single_coord_shape
         new_coords = np.reshape(new_coords, new_shape)
+        if return_derivs:
+            ders = ops['derivs']
+            ders_shape = coords.shape + single_coord_shape
+            ders = ders.reshape(ders_shape)
+            ops['derivs'] = ders
         return new_coords, ops
 
     def convert(self, coords, ordering=None, use_rad=True, return_derivs=False, **kw):
@@ -140,7 +146,7 @@ class CartesianToZMatrixConverter(CoordinateSystemConverter):
         # need to check against the cases of like 1, 2, 3 atom molecules
         # annoying but not hard
         if return_derivs:
-            derivs = np.zeros(coords.shape + (len(ol)-1, 3))
+            derivs = np.zeros(coords.shape + (nol-1, 3))
         if not multiconfig:
             ix = ol[1:, 0]
             jx = ol[1:, 1]
@@ -202,7 +208,9 @@ class CartesianToZMatrixConverter(CoordinateSystemConverter):
             dists = self.get_dists(coords[ix], coords[jx])
             if return_derivs:
                 dist_derivs = dist_deriv(coords, ix, jx)
-                drang = np.arange(len(ix))
+                drang = np.arange(nol-1)
+                nreps = int(len(ix)/(nol-1))
+                drang = np.broadcast_to(drang[:, np.newaxis], drang.shape + (nreps,)).flatten()
                 derivs[ix, :, drang, 0] = dist_derivs[0]
                 derivs[jx, :, drang, 0] = dist_derivs[1]
 
@@ -222,7 +230,9 @@ class CartesianToZMatrixConverter(CoordinateSystemConverter):
                 if return_derivs:
                     # we might need to mess with the masks akin to the insert call...
                     angle_derivs = angle_deriv(coords, ix, jx, kx)
-                    drang = 1+np.arange(len(ix))
+                    drang = 1+np.arange(nol-2)
+                    nreps = int(len(ix)/(nol-2))
+                    drang = np.broadcast_to(drang[:, np.newaxis], drang.shape + (nreps,)).flatten()
                     derivs[ix, :, drang, 1] = angle_derivs[0]
                     derivs[jx, :, drang, 1] = angle_derivs[1]
                     derivs[kx, :, drang, 1] = angle_derivs[2]
@@ -248,7 +258,9 @@ class CartesianToZMatrixConverter(CoordinateSystemConverter):
                     diheds = np.rad2deg(diheds)
                 if return_derivs:
                     dihed_derivs = dihed_deriv(coords, ix, jx, kx, lx)
-                    drang = 2+np.arange(len(ix))
+                    drang = 2+np.arange(nol-2)
+                    nreps = int(len(ix)/(nol-2))
+                    drang = np.broadcast_to(drang[:, np.newaxis], drang.shape + (nreps,)).flatten()
                     derivs[ix, :, drang, 2] = dihed_derivs[0]
                     derivs[jx, :, drang, 2] = dihed_derivs[1]
                     derivs[kx, :, drang, 2] = dihed_derivs[2]

Coordinerds/CoordinateSystems/CoordinateSet.py
@@ -15,8 +15,6 @@ __all__ = [
 ##                                   CoordinateSet Class
 ##
 ######################################################################################################
-
-
 class CoordinateSet(np.ndarray):
     """
     A subclass of np.ndarray that lives in an explicit coordinate system and can convert between them
@@ -103,7 +101,8 @@ class CoordinateSet(np.ndarray):
             ops = {}
         return type(self)(new_coords, system = system, converter_options=ops)
 
-    def jacobian(self, system,
+    def jacobian(self,
+                 system,
                  order=1,
                  coordinates=None,
                  converter_options=None,
@@ -127,10 +126,16 @@ class CoordinateSet(np.ndarray):
         :rtype:
         """
 
+        cops = self.converter_options
+        if cops is None:
+            cops = {}
+        if converter_options is None:
+            converter_options = {}
+        kw = dict(cops, **converter_options)
         return self.system.jacobian(self,
                                     system,
                                     order=order,
                                     coordinates=coordinates,
-                                    converter_options=converter_options,
+                                    converter_options=kw,
                                     **fd_options
                                     )

Coordinerds/CoordinateSystems/ZMatrixToCartesian.py
@@ -6,8 +6,8 @@ import numpy as np
 # what properties its class will have and will try to claim that the files don't exist
 
 class ZMatrixToCartesianConverter(CoordinateSystemConverter):
-    """A converter class for going from ZMatrix coordinates to Cartesian coordinates
-
+    """
+    A converter class for going from ZMatrix coordinates to Cartesian coordinates
     """
 
     @property
@@ -284,6 +284,7 @@ class ZMatrixToCartesianConverter(CoordinateSystemConverter):
         if axes.ndim == 2:
             axes = np.broadcast_to(axes[np.newaxis], (sysnum, 2, 3))
         x_axes = vec_normalize(axes[:, 0])
+
         dists = coordlist[:, 0, 0]
         ref_points_1 = np.reshape(dists, (sysnum, 1)) * x_axes
         ref_points_1 += origins
@@ -299,7 +300,7 @@ class ZMatrixToCartesianConverter(CoordinateSystemConverter):
             ic = None
             R1 = None
             R2 = None
-            v = x_axes
+            v = axes[:, 0]
             u = None
             n = None
             self._fill_deriv(i, derivs, r, q, f, ia, ib, ic, v, u, n, R1, R2)

Zachary/Taylor/Derivatives.py
@@ -354,19 +354,27 @@ class DerivativeGenerator:
         displacements, displaced_coords = disp_data
         stencil_widths, stencil_shapes, finite_difference, dorder = fd_data
 
+        # TODO: sit down and add comments to this compact implementation...
+
         config_shape = self.config_shape
         cdim = len(config_shape)
         roll = [cdim] + [a for a in np.arange(displaced_coords.ndim) if a != cdim]
         dcoords = displaced_coords.transpose(roll)
         function_values = f(dcoords)
-        roll = tuple(np.arange(cdim) + 1) + (0,) + tuple(np.arange(cdim+1, function_values.ndim))
-        function_values = function_values.transpose(roll)
+        unroll = tuple(np.arange(cdim) + 1) + (0,) + tuple(np.arange(cdim+1, function_values.ndim))
+        function_values = function_values.transpose(unroll)
         disp, fvals = self.prep(spec, displacements, function_values)
 
         # TODO: handle stuff like dipoles where we have an x, y, z component each of which should be handled separately...
         #       this might actually be handled naturally, though? I'm actually pretty hopeful it will be...
 
         # we now need to reformat the fvals so that they respect the stencil_shapes
+        # not sure where the extra 1 is coming from here...?
+        # I think maybe from the transpose call...?
+        # Actually no that just puts the displacement back in the right spot...
+        #   seems that it comes out of the way the coordinates get fed in/come out...
+        #   the tested stuff all has an extra '1' because of the way the displacement
+        #   gets generated
         out_shape = fvals.shape[1+len(self.config_shape):]
         out_dim = 1 if isinstance(out_shape, (int, np.integer)) else len(out_shape)
         fvals_shape = self.config_shape + stencil_widths