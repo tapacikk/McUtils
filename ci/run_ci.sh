8c283909d1418a48f81e9784bdf6657d694d88cc
b3m2a1 <b3m2a1@gmail.com>
2021-01-03 15:50:38 -0500

    Changing up docs a bit

--- ci/run_ci.sh
+++ ci/run_ci.sh
@@ -5,18 +5,27 @@ set -e
 ## Runs CI
 ##
 
+branch=$(git rev-parse --abbrev-ref HEAD)
+
 # get into the parent folder and merge in changes from the master branch
 cd /home/McUtils
 git config user.name ${GITHUB_ACTOR}
 git config user.email ${GITHUB_ACTOR}@users.noreply.github.com
 git checkout gh-pages
-git merge edit
+git merge $branch
 ## run the test script
 cd /home
-PYTHONPATH=/home python3 McUtils/ci/tests/run_tests.py -v -d
 
+if [[ "$branch" == "master" ]]; then
+  PYTHONPATH=/home python3 McUtils/ci/tests/run_tests.py -v -d
+else
+  PYTHONPATH=/home python3 McUtils/ci/tests/run_tests.py -d
+fi
+
+if [[ "$branch" == "edit" ]]; then
 # build docs and push
 PYTHONPATH=/home python3 McUtils/ci/build_docs.py
 cd McUtils
 git add -A && git commit -m "Built out docs"
 git push -u "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/McCoyGroup/McUtils.git" gh-pages
+fi
