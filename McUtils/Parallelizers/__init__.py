0caa4643103f8b0f8de4f613cbd9587a611dc145
b3m2a1 <b3m2a1@gmail.com>
2021-11-16 14:10:52 -0800

    Expose runner

--- McUtils/Parallelizers/__init__.py
+++ McUtils/Parallelizers/__init__.py
@@ -3,5 +3,8 @@ Provides utilities for setting up platform-independent parallelism
 in a hopefully unobtrusive way
 """
 
-from .Parallelizers import *
-from .Parallelizers import __all__
+__all__ = []
+from .Parallelizers import *; from .Parallelizers import __all__ as exposed
+__all__ += exposed
+from .Runner import *; from .Runner import __all__ as exposed
+__all__ += exposed
