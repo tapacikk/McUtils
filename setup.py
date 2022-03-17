0787d261d086064567c3a42f7c32e7372df59ff1
b3m2a1 <b3m2a1@gmail.com>
2022-03-17 12:07:54 -0700

    Versioneer unecessarily complicated for Git workflow

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ A simple package for working with common McCoy group problems
 
 # pulled from Ryan's stuff, lightly modified
 from setuptools import setup, find_packages
-import versioneer
 
 short_description = __doc__.split("\n")
 
@@ -15,6 +14,10 @@ try:
 except:
     long_description = "\n".join(short_description[2:])
 
+def get_version():
+    import subprocess
+    run_out = subprocess.run(['git', 'describe', '--tags', '--abbrev=0'], capture_output=True)
+    return run_out.stdout.decode().strip().strip("v")
 
 setup(
     # Self-descriptive entries which should always be present
@@ -24,14 +27,14 @@ setup(
     description=short_description[0],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+    version=get_version(),
+    # cmdclass=versioneer.get_cmdclass(),
     license='MIT',
 
     # Which Python importable modules should be included when your package is installed
     # Handled automatically by setuptools. Use 'exclude' to prevent some specific
     # subpackage(s) from being added, if needed
-    packages=find_packages()
+    packages=find_packages("Psience")
 
     # Optional include package data to ship with your package
     # Customize MANIFEST.in if the general case does not suit your needs
