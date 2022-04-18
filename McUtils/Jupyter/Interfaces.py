61d64c76a56eb641b692b0253c5551732b5c36ed
b3m2a1 <b3m2a1@gmail.com>
2022-04-18 12:36:51 -0700

    adding menus

--- McUtils/Jupyter/Interfaces.py
+++ McUtils/Jupyter/Interfaces.py
@@ -274,6 +274,55 @@ class JHTMLInterface(WidgetInterface):
             # self._widget_cache.to_widget.observe(self.set_value, )
         return self._widget_cache
 
+# class TabBar(JHTMLInterface):
+#     ...
+#
+#
+#
+#     """
+#     <ul class="nav nav-tabs">
+#   <li class="nav-item">
+#     <a class="nav-link active" aria-current="page" href="#">Active</a>
+#   </li>
+#   <li class="nav-item dropdown">
+#     <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Dropdown</a>
+#     <ul class="dropdown-menu">
+#       <li><a class="dropdown-item" href="#">Action</a></li>
+#       <li><a class="dropdown-item" href="#">Another action</a></li>
+#       <li><a class="dropdown-item" href="#">Something else here</a></li>
+#       <li><hr class="dropdown-divider"></li>
+#       <li><a class="dropdown-item" href="#">Separated link</a></li>
+#     </ul>
+#   </li>
+#   <li class="nav-item">
+#     <a class="nav-link" href="#">Link</a>
+#   </li>
+#   <li class="nav-item">
+#     <a class="nav-link disabled">Disabled</a>
+#   </li>
+# </ul>
+#     """
+class Menulike(JHTMLInterface):
+    def __init__(self, *items, item_attrs=None):
+        super().__init__()
+        self.items = items
+        self._item_map = {}
+        self.item_attrs = {} if item_attrs is None else item_attrs
+    @abc.abstractmethod
+    def create_item(self, body=None, id=None, **extra):
+        ...
+    @abc.abstractmethod
+    def wrap_items(self, items):
+        ...
+    def to_jhtml(self, parent=None):
+        items = [self.create_item(**dict(self.item_attrs, **item)) for item in self.items]
+        for i in items:
+            if hasattr(i, 'id'):
+                self._item_map[i.id] = i
+            elif 'id' in i.attrs:
+                self._item_map[i.attrs['id']] = i
+        return self.wrap_items(items)
+
 class Sidebar(JHTMLInterface):
     item_classes = ['list-group-item', 'list-group-item-action']
     wrapper_classes = ['list-group', 'list-group-flush']
@@ -315,6 +364,20 @@ class Sidebar(JHTMLInterface):
             cls=self.base_classes,
             **self.attrs
         )
+class DropdownMenu(JHTMLInterface):
+    def __init__(self, actions):
+        self.actions = actions
+        super().__init__()
+    def create_item(self, body=None, href="#", id=None, action=None):
+        attrs = {'href':href}
+        if action is not None:
+            attrs['event_handlers'] = {'click':action}
+        if body is not None:
+            body = [body]
+        return JHTML.ListItem(
+            JHTML.Anchor(*body, **attrs)
+        )
+
 
 class JHTMLControl(JHTMLInterface):
     layout_orientation = 'row'
@@ -345,6 +408,98 @@ class JHTMLControl(JHTMLInterface):
         if self._widget_cache is None:
             raise ValueError("not initialized")
         return self._widget_cache.to_widget().observe(fn, names=names)
+class ActiveSetter(JHTMLControl):
+    def __init__(self, var, options, item_attrs=None, debug_pane=None, **attrs):
+        super().__init__(var)
+        self.options = options
+        self.item_attrs = {} if item_attrs is None else item_attrs
+        self.attrs = attrs
+        self._element_map = {}
+        self._item_map = {}
+        self._active_item = None
+        self.logger_pane = DefaultOutputArea() if debug_pane is None else debug_pane
+        val = self.options[0]['value']
+        self.var.value = val
+    def get_value(self):
+        return self._item_map[self._active_item]
+    def set_value(self):
+        self.set_active(self.var.value)
+    def update(self, e):
+        self.set_active(self.value)
+    def set_active(self, v):
+        for k, test in self._item_map.items():
+            if test == v:
+                break
+        else:
+            k = None
+
+        if k is not None:
+            if (
+                    self._active_item is not None
+                    and k != self._active_item
+                    and len(self._element_map) > 0
+            ):
+                cur = self._element_map[self._active_item]
+                if cur is not None:
+                    cur.remove_class('active')
+                self._active_item = k
+                new = self._element_map[k]
+                if new is not None:
+                    new.add_class('active')
+    def onclick(self, e, i, v):
+        self.var.set_value(v, caller=self)
+        with self.logger_pane:
+            self.set_active(v)
+
+    @abc.abstractmethod
+    def create_items(self):
+        ...
+
+    # def create_link_dict(self, o, which=-1):
+    #     uid = str(uuid.uuid1()).replace("-", "")
+    #     o = o.copy()
+    #     label = o['label'] if 'label' in o else o['value']
+    #     if 'label' in o:
+    #         del o['label']
+    #     value = o['value']
+    #     self._item_map[uid] = value
+    #     del o['value']
+    #     if which == 0:
+    #         self._active_item = uid
+    #         if 'cls' in o:
+    #             cls = o['cls']
+    #         elif 'cls' in self.item_attrs:
+    #             cls = self.item_attrs['cls']
+    #         else:
+    #             cls = []
+    #         if isinstance(cls, str):
+    #             cls = cls.split()
+    #         o['cls'] = list(cls) + ['active']
+    #     o['id'] = uid
+    #     return dict(
+    #         body=label,
+    #         event_handlers=dict(
+    #             click=lambda *e, i=uid, v=value: self.onclick(e, i, v)
+    #         ),
+    #         **o
+    #     )
+
+    def to_jhtml(self):
+        bar = Sidebar(
+            *[
+                self.create_link_dict(o, which=n)
+                for n, o in enumerate(self.options)
+            ],
+            item_attrs=self.item_attrs,
+            **self.attrs
+        ).to_jhtml()
+
+        col_wrapper = bar.get_child(0, wrapper=True)
+        for i in range(len(col_wrapper.children)):
+            wrapper = col_wrapper.get_child(i, wrapper=True)
+            self._element_map[wrapper['id']] = wrapper
+
+        return bar
 class SidebarSetter(JHTMLControl):
     layout_orientation = 'column'
     def __init__(self, var, options, item_attrs=None, debug_pane=None, **attrs):
@@ -569,39 +724,85 @@ class CardManipulator(WidgetInterface):
     def initialize(self):
         self.output.update()
 
-class AppExplorer(WidgetInterface):
+class Form(WidgetInterface):
 
-    def __init__(self, interfaces):
-        self.interfaces = interfaces
+    def __init__(self, *form_specs, layout_function=None):
+        self.forms = form_specs
+        self.layout = layout_function
 
-    def to_widget(self):
-        interface = JHTML.Bootstrap.Card(
-            JHTML.Bootstrap.CardHeader(
-                "" if self.title is None else self.title
-                # JHTML.SubsubHeading("A Pane with Output", JHTML.Small(" with a slider for fun", cls='text-muted')),
-            ),
-            JHTML.Bootstrap.CardBody(
-                JHTML.Div(
-                    JHTML.Bootstrap.Row(
-                        *(JHTML.Bootstrap.Col(c.to_widget(), cls=["col-1", 'bg-light', 'border-end', 'p-0', 'm-0'])
-                          for c in self.column_controls),
-                        JHTML.Bootstrap.Col(
-                            JHTML.Div(
-                                *[t.to_widget() for t in self.toolbar_controls],
-                                cls=['bg-light', 'border-bottom', 'd-inline-block', 'w-100', "p-2"]
-                                # , style=dict(min_height="80px")#, flex="1")
-                            ),
-                            self.output.to_widget(),
-                            style=dict(min_height='500px')
-                        ),
-                        cls=['g-0', "flex-grow-1"]
-                    ),
-                    style=dict(flex="1"),
-                    cls=["p-0", "d-flex", "flex-column", 'max-width-auto', 'w-100']
-                ),
-                cls=["p-0"]
-            ),
-            JHTML.Bootstrap.CardFooter("" if self.output_pane is None else self.output_pane)
-        )
+    @classmethod
+    def create_control(self,
+                       name=None,
+                       field_type=None,
+                       default_value=None,
+                       control_type=None,
+                       label=True
+                       ):
+        ...
 
-        return interface
+    # def
+
+
+# class GridItem(WidgetInterface):
+#     __slots__ = ['item', "row", "col", 'span_rows', 'span_cols']
+#     def __init__(self, item, row, col, span_rows=1, span_cols=1):
+#         self.item = item
+#         self.row = row
+#         self.col = col
+#         self.span_rows = span_rows
+#         self.span_cols = span_cols
+#     def to_jhtml(self):
+#         return JHTML.Div(
+#             self.item,
+#             cls=
+#         )
+#
+# class Grid(WidgetInterface):
+#     def __init__(self, elements):
+#         self.elements = elements
+#
+#     def compute_spans(self, i, j, span):
+#         ...
+#
+#     def to_jhtml(self):
+#         JHTML.Div(
+#
+#             cls='grid'
+#         )
+
+# class AppExplorer(WidgetInterface):
+#
+#     def __init__(self, interfaces):
+#         self.interfaces = interfaces
+#
+#     def to_widget(self):
+#         interface = JHTML.Bootstrap.Card(
+#             JHTML.Bootstrap.CardHeader(
+#                 "" if self.title is None else self.title
+#                 # JHTML.SubsubHeading("A Pane with Output", JHTML.Small(" with a slider for fun", cls='text-muted')),
+#             ),
+#             JHTML.Bootstrap.CardBody(
+#                 JHTML.Div(
+#                     JHTML.Bootstrap.Row(
+#                         *(JHTML.Bootstrap.Col(c.to_widget(), cls=["col-1", 'bg-light', 'border-end', 'p-0', 'm-0'])
+#                           for c in self.column_controls),
+#                         JHTML.Bootstrap.Col(
+#                             JHTML.Div(
+#                                 *[t.to_widget() for t in self.toolbar_controls],
+#                                 cls=['bg-light', 'border-bottom', 'd-inline-block', 'w-100', "p-2"]
+#                                 # , style=dict(min_height="80px")#, flex="1")
+#                             ),
+#                             self.output.to_widget(),
+#                             style=dict(min_height='500px')
+#                         ),
+#                         cls=['g-0', "flex-grow-1"]
+#                     ),
+#                     style=dict(flex="1"),
+#                     cls=["p-0", "d-flex", "flex-column", 'max-width-auto', 'w-100']
+#                 ),
+#                 cls=["p-0"]
+#             ),
+#             JHTML.Bootstrap.CardFooter("" if self.output_pane is None else self.output_pane)
+#         )
+#
+#         return interface
