f35ff04763b600037a7972842bb4ce7b8ecdc0c1
b3m2a1 <b3m2a1@gmail.com>
2022-05-05 20:37:05 -0700

    Update __init__.py

--- McUtils/__init__.py
+++ McUtils/__init__.py
@@ -11,6 +11,7 @@ been written over the years.
 import McUtils.Data
 import McUtils.Parsers
 import McUtils.GaussianInterface
+import McUtils.Misc
 import McUtils.Jupyter
 import McUtils.Plots
 import McUtils.Zachary
@@ -30,5 +31,6 @@ __all__ = [
     "Numputils",
     "Scaffolding",
     "Parallelizers",
-    "Jupyter"
+    "Jupyter",
+    "Misc"
 ]
