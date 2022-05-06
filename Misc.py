84549d676176e5b8a4187b8c032f7d88d1823993
b3m2a1 <b3m2a1@gmail.com>
2022-05-05 20:38:26 -0700

    Create Misc.py

---
+++ Misc.py
@@ -0,0 +1,8 @@
+"""
+A file that only exists to make this directory useable as a way to import McUtils too.
+Mostly useful during development
+"""
+
+from .McUtils.Misc import *
+from .McUtils.Misc import __all__
+from .McUtils.Misc import __doc__
