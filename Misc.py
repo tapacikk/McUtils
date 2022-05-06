d010b878cf343dae2faea7eadf8478e4d34996b7
b3m2a1 <b3m2a1@gmail.com>
2022-05-05 18:19:59 -0700

    Update binder and docs stuff

--- Misc.py
+++
@@ -1,8 +0,0 @@
-"""
-A file that only exists to make this directory useable as a way to import McUtils too.
-Mostly useful during development
-"""
-
-from .McUtils.Misc import *
-from .McUtils.Misc import __all__
-from .McUtils.Misc import __doc__
